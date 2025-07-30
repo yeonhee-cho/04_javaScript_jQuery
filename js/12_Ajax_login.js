1dad96ffafa5df4e823e1d85417c0818bd4e1716
yeonhee-cho <yhcho.proj@gmail.com>
2025-07-30 16:08:03 +0900

    [Add] Ajax를 활용한 내용 - 존재하는 아이디가 없을 때의 오류 수정 + 로그아웃

--- js/12_Ajax_login.js
+++ js/12_Ajax_login.js
@@ -35,14 +35,23 @@ $(function () {
           예를 들어 username.value값으로 admin이 들어왔을 경우
           users[abc] -> user[admin] 으로 변경되어 admin과 일치하는 아이디를 검색
         */
-        if (data.users[abc].password === password) {
-          $("#result").html(
-            `로그인 성공! 환영합니다. ${data.users[abc].name} 님`
-          );
-          $("#로그인기능").hide();
-          $("#로그아웃기능").show();
+
+        // abc가 존재하는가 확인 먼저 하기!!
+        if (data.users[abc]) {
+          // 사용자가 존재할 때
+          if (data.users[abc].password === password) {
+            $("#result").html(
+              `로그인 성공! 환영합니다. ${data.users[abc].name} 님`
+            );
+            $("#로그인기능").hide();
+            $("#로그아웃기능").show();
+          } else {
+            // 사용자가 존재하지만, 비밀번호가 일치하지 않을 때
+            $("#result").html("일치하는 아이디나 비밀번호가 없습니다.");
+          }
         } else {
-          $("#result").html("일치하는 아이디나 비밀번호가 없습니다.");
+          // 사용자가 존재하지 않을 때
+          $("#result").html("존재하는 아이디가 없습니다.");
         }
       },
       error: function () {
@@ -51,9 +60,15 @@ $(function () {
     });
   });
 
-  $("로그아웃기능").click(function () {
+  $("#로그아웃기능").click(function (e) {
     // 로그아웃을 진행할 경우
     // 로그인기능 show 보여주기
     // 로그아웃기능 hide 숨기기 설정
+    e.preventDefault(); // 로그아웃 button 태그 내부에 type = submit 로 설정되어 있어, 제출방지
+    $("#로그인기능").show();
+    $("#로그아웃기능").hide();
+    $("#username").val("");
+    $("#password").val("");
+    $("#result").html("로그아웃이 완료되었습니다.");
   });
 });
