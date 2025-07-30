ec687823ff8a5491db033193741bbdc117bb5fa0
yeonhee-cho <yhcho.proj@gmail.com>
2025-07-30 17:19:52 +0900

    [Add] 도서검색 = 입력하지 않았을 경우 체크

--- js/13_Ajax_GET_도서검색.js
+++ js/13_Ajax_GET_도서검색.js
@@ -29,38 +29,45 @@ $(function () {
       // 2. author로 저자 값 val() 가져오기
       const author = $("#author").val();
 
-      if (data.books[bookTitle]) {
-        // 책제목이 존재한다면
-        console.log("책제목이 존재한다면");
+      // 입력하지 않았을 경우 체크
+      console.log("bookTitle", bookTitle, "author", author);
 
-        if (data.books[bookTitle].author === author) {
-          // JSON 데이터에 저장된 저자와 소비자가 검색한 저자가 일치하는 항목이 존재한다면
-          console.log(
-            "JSON 데이터에 저장된 저자와 소비자가 검색한 저자가 일치하는 항목이 존재한다면"
-          );
-          $("#result").removeClass("error");
-          $("#result").addClass("success");
-          $("#result").html(`
-                    <div class="book-info">
-                        <h3>도서찾기 성공!</h3>
-                        <p><strong>제목 : </strong> ${bookTitle}</p>
-                        <p><strong>저자 : </strong> ${author}</p>
-                        <p><strong>가격 : </strong> ${data.books[bookTitle].price}</p>
-                    </div>
-                    `);
-          // 도서 검색 버튼 숨기기
-          $("#도서검색").hide();
-          // 검색 초기화 버튼 보이기
-          $("#검색초기화").show();
-        } else {
-          // 없을 경우
-          $("#result").html(
-            `${bookTitle} 책의 저자가 ${author}과 일치하는 책이 존재하지 않습니다.`
-          );
-        }
+      if (bookTitle == "" || author == "") {
+        $("#result").html("찾으시려는 도서명과 저자를 입력하세요");
       } else {
-        // 책 제목이 존재하지 않는다면
-        $("#result").html(`${bookTitle}과 일치하는 책이 존재하지 않습니다.`);
+        if (data.books[bookTitle]) {
+          // 책제목이 존재한다면
+          console.log("책제목이 존재한다면");
+
+          if (data.books[bookTitle].author === author) {
+            // JSON 데이터에 저장된 저자와 소비자가 검색한 저자가 일치하는 항목이 존재한다면
+            console.log(
+              "JSON 데이터에 저장된 저자와 소비자가 검색한 저자가 일치하는 항목이 존재한다면"
+            );
+            $("#result").removeClass("error");
+            $("#result").addClass("success");
+            $("#result").html(`
+                  <div class="book-info">
+                      <h3>도서찾기 성공!</h3>
+                      <p><strong>제목 : </strong> ${bookTitle}</p>
+                      <p><strong>저자 : </strong> ${author}</p>
+                      <p><strong>가격 : </strong> ${data.books[bookTitle].price}</p>
+                  </div>
+                  `);
+            // 도서 검색 버튼 숨기기
+            $("#도서검색").hide();
+            // 검색 초기화 버튼 보이기
+            $("#검색초기화").show();
+          } else {
+            // 없을 경우
+            $("#result").html(
+              `${bookTitle} 책의 저자가 ${author}과 일치하는 책이 존재하지 않습니다.`
+            );
+          }
+        } else {
+          // 책 제목이 존재하지 않는다면
+          $("#result").html(`${bookTitle}과 일치하는 책이 존재하지 않습니다.`);
+        }
       }
     });
   });
