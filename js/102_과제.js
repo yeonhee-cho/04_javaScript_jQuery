abb82fcd6ec61bc9f7aed580d5b931c16c94c0bd
yeonhee-cho <yhcho.proj@gmail.com>
2025-08-03 13:45:20 +0900

    [Add] 과제 정리, 모달 생성

--- js/102_과제.js
+++ js/102_과제.js
@@ -1,16 +1,16 @@
 // https://api.disneyapi.dev/character/308
 
-let 현재페이지 = 1;
-let 전체게시물 = [];
+let currentPage = 1;
+let totalList = [];
 
-const 페이지당게시물수 = 4;
+const pageListNum = 4;
 
 $(function () {
   getAllPosts();
 
   $("#prevBtn").click(() => {
-    if (현재페이지 > 1) {
-      현재페이지--;
+    if (currentPage > 1) {
+      currentPage--;
       getPosts();
     } else {
       alert("첫 페이지입니다.");
@@ -18,9 +18,9 @@ $(function () {
   });
 
   $("#nextBtn").click(() => {
-    const 총페이지수 = Math.ceil(전체게시물.length / 페이지당게시물수);
-    if (현재페이지 < 총페이지수) {
-      현재페이지++;
+    const totalPageNum = Math.ceil(totalList.length / pageListNum);
+    if (currentPage < totalPageNum) {
+      currentPage++;
       getPosts();
     } else {
       alert("마지막 페이지입니다.");
@@ -31,38 +31,62 @@ $(function () {
 // 데이터 전체
 function getAllPosts() {
   $.get("https://api.disneyapi.dev/character").done(function (data) {
-    전체게시물 = data.data;
+    totalList = data.data;
     getPosts();
   });
 }
 
 // 해당 페이지 게시물
 function getPosts() {
-  const 시작하는게시물 = (현재페이지 - 1) * 페이지당게시물수;
-  const 끝인덱스 = 시작하는게시물 + 페이지당게시물수;
-  const 현재페이지게시물 = 전체게시물.slice(시작하는게시물, 끝인덱스);
+  const startIndex = (currentPage - 1) * pageListNum;
+  const lastIndex = startIndex + pageListNum;
+  const currentPageList = totalList.slice(startIndex, lastIndex);
 
   // 페이지 정보 업데이트
-  const 총페이지수 = Math.ceil(전체게시물.length / 페이지당게시물수);
-  $("#pageNum").html(`페이지 ${현재페이지} / ${총페이지수} `);
+  const totalPageNum = Math.ceil(totalList.length / pageListNum);
+  $("#pageNum").html(`${currentPage} / ${totalPageNum} `);
 
   // TODO 페이지 7 imageUrl 404오류 확인 필요!!
-  $("#result").html(
-    현재페이지게시물
-      .map((post) => {
-        const imageCell = post.imageUrl
-          ? `<img src="${post.imageUrl}" alt="${post.name}" />`
-          : `<span class="no-image">해당되는 이미지가 없습니다.</span>`;
+  setTimeout(() => {
+    $("#result").html(
+      currentPageList
+        .map((post) => {
+          const imageCell = post.imageUrl
+            ? `<img src="${post.imageUrl}" 
+            alt="${post.name}"  
+            onclick="openModal('${post.imageUrl}', '${post.name}')"
+            />` // TODO 이미지 이름에 있는 " ' " 특수문자 오류 잡기
+            : `<span class="no-image">해당되는 이미지가 없습니다.</span>`;
 
-        return ` 
-        <tr>
-            <td>
-                ${imageCell}
-            </td>
-            <td><p class="character-name">${post.name}</p></td>
-        </tr>
-        `;
-      })
-      .join("")
-  );
+          return ` 
+            <tr>
+                <td>
+                    ${imageCell}
+                </td>
+                <td><p class="character-name">${post.name}</p></td>
+            </tr>
+            `;
+        })
+        .join("")
+    );
+  }, 200);
 }
+
+// 이미지 클릭 시 모달창 띄우기
+function openModal(imageUrl, name) {
+  $("#modalOverlay").fadeIn(300);
+
+  $("#imgDetail").html(`<img src="${imageUrl}" alt="${name}" />`);
+}
+
+// 모달 닫기
+$("#closeModal").click(() => {
+  $("#modalOverlay").fadeOut(300);
+});
+
+// 외부 클릭 시 모달 닫기
+$("#modalOverlay").click((e) => {
+  if (e.target === this) {
+    $("#modalOverlay").fadeOut();
+  }
+});
