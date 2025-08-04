d0fbf8234c582004ed05e46b9b54e3dc5700ef0b
yeonhee-cho <yhcho.proj@gmail.com>
2025-08-04 18:04:39 +0900

    [Add] 이메일 체크 확인 시작

---
+++ js/24_signup.js
@@ -0,0 +1,19 @@
+$(function () {
+  // 1. 중복확인 버튼 클릭 시 팝업 열기
+  $("#emailCheck").click(function () {
+    // 팝업 설정
+    // const popupWidth = ???;
+    // const popupHeight = ???;
+    // TODO: 화면 중앙에 팝업 띄우기
+  });
+
+  // 2. 회원가입 폼 제출
+  $("#signupForm").submit(function (e) {
+    e.preventDefault();
+
+    // TODO:
+    // - 입력값 가져오기
+    // - localStorage에 저장
+    // - 성공 메시지 표시
+  });
+});
