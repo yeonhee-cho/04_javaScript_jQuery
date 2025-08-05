5cd42b1ba5ae235e1d1dd19d501ac9850b09f5c1
yeonhee-cho <yhcho.proj@gmail.com>
2025-08-05 15:47:32 +0900

    [Add] open opener를 활용한 아이디 중복확인

--- js/24_emailCheck.js
+++ js/24_emailCheck.js
@@ -3,17 +3,38 @@ $(function () {
   $("#check").click(function () {
     const email = $("#childEmail").val();
 
-    // TODO:
     // - localStorage에서 users 가져오기
     // - 중복 확인
+    let userList = JSON.parse(localStorage.getItem("users") || "[]");
+    const isDup = userList.filter((u) => u.email === email);
+
+    // filter는 배열로 데이터를 가져오기 때문에 .length로 값이 한 개라도 존재하는가 //TODO
+    if (isDup.length > 0) {
+      $("#result").html(
+        `<span style="color:red;">이미 사용 중인 이메일입니다.</span>`
+      );
+      $("#send").prop("disabled", true);
+    } else {
+      $("#result").html(
+        `<span style="color:green;">사용 가능한 이메일입니다.</span>`
+      );
+      $("#send").prop("disabled", false);
+    }
     // - 결과 메시지 표시
     // - 중복되지 않으면 "사용하기" 버튼 활성화
   });
 
   // 2. 사용하기 버튼 클릭
   $("#send").click(function () {
-    // TODO:
-    // - 부모창의 이메일 입력란에 값 전달
-    // - 팝업창 닫기
+    // 부모창의 이메일 입력란에 값 전달
+    // 순수 자바스크립트와 jquery랑 언제든지 혼용해서 사용 가능하다.
+    // 순수 자바스크립트 조합                                    jquery조합
+    // opener.document.getElementById("inputEmail").value = $("#childEmail").val();
+
+    // jquery 조합                      jquery조합
+    // opener.$("#inputEmail").val() = $("#childEmail").val();
+    opener.$("#inputEmail").val($("#childEmail").val());
+    // 팝업창 닫기
+    window.close();
   });
 });
--- js/24_signup.js
+++ js/24_signup.js
@@ -2,9 +2,20 @@ $(function () {
   // 1. 중복확인 버튼 클릭 시 팝업 열기
   $("#emailCheck").click(function () {
     // 팝업 설정
-    // const popupWidth = ???;
-    // const popupHeight = ???;
-    // TODO: 화면 중앙에 팝업 띄우기
+    const popupWidth = 450;
+    const popupHeight = 450;
+    // 화면 중앙에 팝업 띄우기
+    const popupLeft = (window.screen.width - popupWidth) / 2;
+    const popupTop = (window.screen.height - popupHeight) / 2;
+
+    const options = `
+    width = ${popupWidth},
+    height = ${popupHeight},
+    left = ${popupLeft},
+    top = ${popupTop},
+    `;
+    // 팝업
+    window.open("24_emailCheck.html", "_blank", options.toString());
   });
 
   // 2. 회원가입 폼 제출
